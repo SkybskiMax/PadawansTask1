dd93377541a238f05668ccdaf07fc81120f167e8
Max <dranik15@mail.ru>
2019-02-15 19:07:45 +0300

    Update Population.cs

--- Population.cs
+++ Population.cs
@@ -6,8 +6,16 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation<=0 || currentPopulation<=0)
+            {
+                throw new ArgumentException();
+            }
+            int years = 0;
+            for (; initialPopulation < currentPopulation ; years++)
+            {
+                initialPopulation = initialPopulation + (int)(initialPopulation * percent * 0.01) + visitors;
+            }
+            return years;
         }
     }
 }
